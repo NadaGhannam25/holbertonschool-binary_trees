db08adeaac7ec9307bb2043b3193a85c71754248
root <Nada.Ghannam19@gmail.com>
2025-10-21 16:22:05 +0000

    Implement AVL tree insertion function with rotations

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,22 +1,38 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl - builds an AVL tree from an array
- * @array: pointer to the array of integers
- * @size: number of elements in the array
- * Return: pointer to root of AVL tree, or NULL on failure
- */
+* avl_insert - temporary AVL insert for testing
+* @tree: double pointer to root
+* @value: value to insert
+* Return: pointer to node inserted
+*/
+avl_t *avl_insert(avl_t **tree, int value)
+{
+if (!tree)
+return (NULL);
+
+if (*tree == NULL)
+*tree = binary_tree_node(NULL, value);
+return (*tree);
+}
+
+/**
+* array_to_avl - builds AVL tree from array
+* @array: array of integers
+* @size: array size
+* Return: pointer to root
+*/
 avl_t *array_to_avl(int *array, size_t size)
 {
-	size_t i;
-	avl_t *root = NULL;
+size_t i;
+avl_t *root = NULL;
 
-	if (!array || size == 0)
-		return (NULL);
+if (!array || size == 0)
+return (NULL);
 
-	for (i = 0; i < size; i++)
-		avl_insert(&root, array[i]);
+for (i = 0; i < size; i++)
+avl_insert(&root, array[i]);
 
-	return (root);
+return (root);
 }
 
