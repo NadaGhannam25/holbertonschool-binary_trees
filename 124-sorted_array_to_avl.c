95dec965442a41078dfb2a8422a6cb497a45df02
root <Nada.Ghannam19@gmail.com>
2025-10-21 16:56:41 +0000

    Implement function to build AVL tree from sorted array

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -5,33 +5,26 @@
 * @array: Pointer to the array of integers
 * @start: Start index of the current subarray
 * @end: End index of the current subarray
-* 
 * Return: Pointer to the root of the subtree
 */
 static avl_t *sorted_array_to_avl_rec(int *array, int start, int end)
 {
 int mid;
 avl_t *node;
-
 if (start > end)
 return (NULL);
-
 mid = (start + end) / 2;
 node = binary_tree_node(NULL, array[mid]);
 if (!node)
 return (NULL);
-
 node->left = sorted_array_to_avl_rec(array, start, mid - 1);
 if (node->left)
 node->left->parent = node;
-
 node->right = sorted_array_to_avl_rec(array, mid + 1, end);
 if (node->right)
 node->right->parent = node;
-
 return (node);
 }
-
 /**
 * sorted_array_to_avl - Builds an AVL tree from a sorted array
 * @array: Pointer to the array of integers
@@ -43,7 +36,5 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 if (!array || size == 0)
 return (NULL);
-
 return (sorted_array_to_avl_rec(array, 0, (int)size - 1));
 }
-
