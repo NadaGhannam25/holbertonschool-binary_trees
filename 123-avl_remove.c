a0b6396266680c8a74ea14bf140ef0118a378a00
oDoDyK <4thkiller2005@gmail.com>
2025-10-22 09:31:06 +0000

    Implement AVL node removal with rebalancing

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -2,72 +2,65 @@
 #include <stdlib.h>
 
 /**
-* find_min - Finds the in-order successor of a node
-* @node: pointer to the node
-* Return: pointer to successor
-*/
+ * find_min - Finds the in-order successor
+ */
 avl_t *find_min(avl_t *node)
 {
-while (node && node->left)
-node = node->left;
-return (node);
+    while (node && node->left)
+        node = node->left;
+    return (node);
 }
 
 /**
-* avl_remove - Remove a node from AVL tree and rebalance
-* @root: pointer to root node
-* @value: value to remove
-* Return: pointer to new root
-*/
+ * avl_remove - Remove a node from AVL tree and rebalance
+ */
 avl_t *avl_remove(avl_t *root, int value)
 {
-avl_t *tmp;
-int balance;
+    avl_t *tmp;
+    int balance;
 
-if (!root)
-return (NULL);
+    if (root == NULL)
+        return (NULL);
 
-if (value < root->n)
-root->left = avl_remove(root->left, value);
-else if (value > root->n)
-root->right = avl_remove(root->right, value);
-else
-{
-if (!root->left || !root->right)
-{
-tmp = root->left ? root->left : root->right;
-free(root);
-return (tmp);
-}
-tmp = find_min(root->right);
-root->n = tmp->n;
-root->right = avl_remove(root->right, tmp->n);
-}
+    if (value < root->n)
+        root->left = avl_remove(root->left, value);
+    else if (value > root->n)
+        root->right = avl_remove(root->right, value);
+    else
+    {
+        // Node found
+        if (!root->left || !root->right)
+        {
+            tmp = root->left ? root->left : root->right;
+            free(root);
+            return (tmp);
+        }
+        tmp = find_min(root->right);
+        root->n = tmp->n;
+        root->right = avl_remove(root->right, tmp->n);
+    }
 
-/* لا تحاول إعادة التوازن إذا root أصبح NULL */
-if (!root)
-return (NULL);
+    // Update balance
+    balance = binary_tree_balance(root);
 
-balance = binary_tree_balance(root);
+    if (balance > 1 && binary_tree_balance(root->left) >= 0)
+        return (binary_tree_rotate_right(root));
 
-/* Left heavy */
-if (balance > 1)
-{
-if (binary_tree_balance(root->left) >= 0)
-return (binary_tree_rotate_right(root));
-root->left = binary_tree_rotate_left(root->left);
-return (binary_tree_rotate_right(root));
-}
+    if (balance > 1 && binary_tree_balance(root->left) < 0)
+    {
+        root->left = binary_tree_rotate_left(root->left);
+        return (binary_tree_rotate_right(root));
+    }
 
-/* Right heavy */
-if (balance < -1)
-{
-if (binary_tree_balance(root->right) <= 0)
-return (binary_tree_rotate_left(root));
-root->right = binary_tree_rotate_right(root->right);
-return (binary_tree_rotate_left(root));
-}
+    if (balance < -1 && binary_tree_balance(root->right) <= 0)
+        return (binary_tree_rotate_left(root));
 
-return (root);
+    if (balance < -1 && binary_tree_balance(root->right) > 0)
+    {
+        root->right = binary_tree_rotate_right(root->right);
+        return (binary_tree_rotate_left(root));
+    }
+
+    return (root);
 }
 
